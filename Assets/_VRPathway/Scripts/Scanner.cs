71e4e9b26ad2012ea3b28f51398f4480ed0675bb
Olivier3456 <olivier.larrey@gmail.com>
2023-04-20 08:28:40 +0200

    Scanner's line renderer now on & off when trigger pressed, and screen deploying when scanner is grabbed

--- Assets/_VRPathway/Scripts/Scanner.cs
+++ Assets/_VRPathway/Scripts/Scanner.cs
@@ -7,10 +7,42 @@ public class Scanner : XRGrabInteractable
 {
     [Header("Scanner Data")]
     public Animator animator;
+    public LineRenderer laserRenderer;
 
-  
+
+    protected override void Awake()
+    {
+        base.Awake();
+        laserRenderer.gameObject.SetActive(false);
+    }
+
+
+
     protected override void OnSelectEntered(SelectEnterEventArgs args)
     {
+        base.OnSelectEntered(args);
         animator.SetBool("Opened", true);
     }
+
+
+    protected override void OnSelectExited(SelectExitEventArgs args)
+    {
+        base.OnSelectExited(args);
+        animator.SetBool("Opened", false);
+    }
+
+
+    protected override void OnActivated(ActivateEventArgs args)
+    {
+        base.OnActivated(args);
+        laserRenderer.gameObject.SetActive(true);
+    }
+
+
+    protected override void OnDeactivated(DeactivateEventArgs args)
+    {
+        base.OnDeactivated(args);
+        laserRenderer.gameObject.SetActive(false);
+    }
+
 }
